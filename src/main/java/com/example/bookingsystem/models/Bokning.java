18c188d032a2281e335d5929f9b7de174cef7fd2
faofficial <astromfelicia98@gmail.com>
2025-05-19 13:30:39 +0200

    implemented bokning

--- src/main/java/com/example/bookingsystem/models/Bokning.java
+++ src/main/java/com/example/bookingsystem/models/Bokning.java
@@ -1,4 +1,30 @@
 package com.example.bookingsystem.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Bokning {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name="kundId", referencedColumnName = "id")
+    private Kund kund;
+
+    @ManyToOne
+    @JoinColumn(name="rumId", referencedColumnName = "id")
+    private Rum rum;
 }
