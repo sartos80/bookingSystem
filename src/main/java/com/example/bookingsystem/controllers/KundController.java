d51e4176dde0eb77e0ed4b64b8e4d51960e40702
sara salim <sarasalim80@yahoo.com>
2025-05-20 12:28:35 +0200

    har fixat class DetaljerKundDto

--- src/main/java/com/example/bookingsystem/controllers/KundController.java
+++ src/main/java/com/example/bookingsystem/controllers/KundController.java
@@ -1,4 +1,21 @@
 package com.example.bookingsystem.controllers;
 
+import com.example.bookingsystem.models.Kund;
+import com.example.bookingsystem.repo.KundRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@       Controller
+@RequiredArgsConstructor
 public class KundController {
+    private final KundRepo kundRepo;
+    @RequestMapping("kunder")
+    public List<Kund>getAllKunder(){
+        return kundRepo.findAll();
+
+    }
 }
--- src/main/java/com/example/bookingsystem/dtos/DetaljerKundDto.java
+++ src/main/java/com/example/bookingsystem/dtos/DetaljerKundDto.java
@@ -1,4 +1,21 @@
 package com.example.bookingsystem.dtos;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DetaljerKundDto {
+    private Long id;
+    private String name;
+    private String epost;
+    private String telefonnummer;
+    private BokningDto bokningar; // List of minimal booking DTOs
+
+    // Constructors, getters, setters
 }
+
