d997373d9a74a3a30a2941017424024d150404ea
sara salim <sarasalim80@yahoo.com>
2025-05-20 10:26:39 +0200

    har lagt mer annotering

--- src/main/java/com/example/bookingsystem/models/Kund.java
+++ src/main/java/com/example/bookingsystem/models/Kund.java
@@ -1,13 +1,19 @@
 package com.example.bookingsystem.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 import java.util.List;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Kund {
     @Id
     @GeneratedValue
--- src/main/java/com/example/bookingsystem/services/impl/KundServiceImpl.java
+++ src/main/java/com/example/bookingsystem/services/impl/KundServiceImpl.java
@@ -1,4 +1,7 @@
 package com.example.bookingsystem.services.impl;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class KundServiceImpl {
 }
