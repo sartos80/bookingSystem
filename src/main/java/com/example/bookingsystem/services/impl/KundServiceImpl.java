98f21c2ba5d2f0c4f6ef09d438a539b9ad4ebc21
sara salim <sarasalim80@yahoo.com>
2025-05-20 13:19:45 +0200

    har lagt @Builder

--- src/main/java/com/example/bookingsystem/services/impl/KundServiceImpl.java
+++ src/main/java/com/example/bookingsystem/services/impl/KundServiceImpl.java
@@ -1,8 +1,20 @@
 package com.example.bookingsystem.services.impl;
 
+import com.example.bookingsystem.dtos.DetaljerKundDto;
+import com.example.bookingsystem.dtos.KundDto;
+import com.example.bookingsystem.models.Kund;
+import com.example.bookingsystem.services.KundService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class KundServiceImpl {
+public class KundServiceImpl implements KundService {
+    @Override
+    public KundDto kundToKundDto(Kund k) {
+        return KundDto.builder().id(k.getId()).name(k.getName()).build();
+    }
 
+    @Override
+    public DetaljerKundDto kundToDetaljerKundDto(Kund k) {
+        return null;
+    }
 }
