83d7bf948511b88358de62f2c8f5889c4836dd5a
JoakimSkappegard <joakim_skappegard@hotmail.com>
2025-05-22 01:51:55 +0200

    Börjat titta på controller. Behöver plugga mer...

--- src/main/java/com/example/bookingsystem/controllers/RumController.java
+++ src/main/java/com/example/bookingsystem/controllers/RumController.java
@@ -1,4 +1,16 @@
 package com.example.bookingsystem.controllers;
 
+
+import com.example.bookingsystem.repo.RumRepo;
+import com.example.bookingsystem.services.RumService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RumController {
+
+    private final RumRepo rumRepo;
+
+    RumController(RumRepo rumRepo) {
+        this.rumRepo = rumRepo;
+    }
 }
