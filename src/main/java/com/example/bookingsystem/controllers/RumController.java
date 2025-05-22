400bc8cf94ee7e37d408e738165953e7d2b334bb
sara salim <sarasalim80@yahoo.com>
2025-05-22 22:05:39 +0200

    fixat annotering till RumController

--- src/main/java/com/example/bookingsystem/controllers/RumController.java
+++ src/main/java/com/example/bookingsystem/controllers/RumController.java
@@ -2,15 +2,14 @@ package com.example.bookingsystem.controllers;
 
 
 import com.example.bookingsystem.repo.RumRepo;
+import com.example.bookingsystem.services.KundService;
 import com.example.bookingsystem.services.RumService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequiredArgsConstructor
 public class RumController {
-
-    private final RumRepo rumRepo;
-
-    RumController(RumRepo rumRepo) {
-        this.rumRepo = rumRepo;
-    }
-}
+    private final RumService rumService;
