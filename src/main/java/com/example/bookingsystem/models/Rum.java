9ec261a086d4e79d5dee7b904985ddcac7cb9e5e
sara salim <sarasalim80@yahoo.com>
2025-05-26 18:54:56 +0200

    har ändrat i rumvalidering för att ha bara två typer (enkel eller dubbel rum)

--- src/main/java/com/example/bookingsystem/models/Rum.java
+++ src/main/java/com/example/bookingsystem/models/Rum.java
@@ -1,6 +1,7 @@
 package com.example.bookingsystem.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -21,22 +22,6 @@ public class Rum {
     @GeneratedValue
     private long id;
 
-   /* // Antal ordinarie sängplatser : 1 till 4
-    @Min(value = 1, message = "capacity must be at least 1")
-    @Max(value = 4, message = "capacity cannot be more than  4")
-    private int capacity;
-    // typ av rum (Enkelrum ,  Dubbelrum , familyjerum)
-    @NotBlank(message = "Romm type must not be empty")
-    private String type;
-    // Antal tillåtna extra sängplatser : 0 till 1
-    @Min(value = 0, message = "MaxExtraBeds must be at least 0")
-    @Max(value = 1, message = "MaxExtraBeds cannot be more than  1")
-    private int MaxExtraBeds;
-
-    @OneToMany(mappedBy = "rum", cascade = CascadeType.ALL)
-    private List<Bokning> bokningar;
-}*/
-// Enkelrum = 1, Dubbelrum = 2
    @Min(value = 1, message = "Capacity must be at least 1")
    @Max(value = 4, message = "Capacity cannot be more than 4")
    private int capacity;
@@ -45,11 +30,13 @@ public class Rum {
     @Enumerated(EnumType.STRING)
     private RumTyp type;
 
-    // Endast dubbelrum får ha en extrasäng
     @Min(value = 0, message = "Max extra beds must be at least 0")
-    @Max(value = 1, message = "Max extra beds cannot be more than 1")
+    @Max(value = 2, message = "Max extra beds cannot be more than 2")
     private int maxExtraBeds;
 
     @OneToMany(mappedBy = "rum", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Bokning> bokningar = new ArrayList<>();
+
+
 }
+
