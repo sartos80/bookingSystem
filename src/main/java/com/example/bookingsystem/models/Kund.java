11ba5d32e9fcb4dac83d0ad97714d9720670496c
faofficial <astromfelicia98@gmail.com>
2025-05-26 11:40:52 +0200

    fix validering

--- src/main/java/com/example/bookingsystem/models/Kund.java
+++ src/main/java/com/example/bookingsystem/models/Kund.java
@@ -1,6 +1,10 @@
 package com.example.bookingsystem.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,8 +22,17 @@ public class Kund {
     @Id
     @GeneratedValue
     private Long id;
+    @NotEmpty(message = "Please enter your name")
+    @Size(min = 2, max = 50)
+    @Pattern(regexp = "^[A-ZazÅÄÖåäö]+$", message = "Name can only contain letters")
     private String name;
+    @NotEmpty
+    @Size(min = 2, max = 50)
+    @Email
     private String epost;
+    @NotEmpty
+    @Size(min = 2, max = 20)
+    @Pattern(regexp = "^[0-9\\-\\+\\s]{7,15}$", message = "Phonenumber can only contain numbers between the size 7-15")
     private String telefonnummer;
 
     @OneToMany(mappedBy = "kund", cascade = CascadeType.ALL)
