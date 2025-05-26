7fbf1abf8e151a31d0337649e8fe3e653bbf4cb4
sara salim <sarasalim80@yahoo.com>
2025-05-26 15:14:31 +0200

    har fixat RumValidering

--- src/main/java/com/example/bookingsystem/models/Rum.java
+++ src/main/java/com/example/bookingsystem/models/Rum.java
@@ -1,6 +1,9 @@
 package com.example.bookingsystem.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 
@@ -16,9 +19,16 @@ public class Rum {
     @GeneratedValue
     private long id;
 
-    //1 3 eller 4
+    // Antal ordinarie sängplatser : 1 till 4
+    @Min(value = 1, message = "capacity must be at least 1")
+    @Max(value = 4, message = "capacity cannot be more than  2")
     private int capacity;
+    // typ av rum (Enkelrum eller Dubbelrum)
+    @NotBlank(message = "Romm type must not be empty")
     private String type;
+    // Antal tillåtna extra sängplatser : 0 till 1
+    @Min(value = 0, message = "MaxExtraBeds must be at least 0")
+    @Max(value = 1, message = "MaxExtraBeds cannot be more than  1")
     private int MaxExtraBeds;
 
     @OneToMany(mappedBy = "rum", cascade = CascadeType.ALL)
